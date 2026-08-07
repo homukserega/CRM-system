e85813a7c02623f83e8038ca2383f2b32e3f38d4
Сергей Хомук <homuk.serega@yandex.by>
2026-08-07 22:08:25 +0300

    registration and authorization has been added

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import RegisterView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(http_method_names=['get', 'post']), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
+from django.contrib.auth.models import Group
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/login.html'
+    success_url = reverse_lazy('accounts:login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Добавляем пользователя в группу "Оператор" (по умолчанию)
+        group, _ = Group.objects.get_or_create(name='Оператор')
+        self.object.groups.add(group)
+        return response
